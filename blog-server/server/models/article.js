2796e18f4694d38906b204393d640ecb3e26ac51
chris <chrisshen93@gmail.com>
2018-04-07 00:26:14 +0800

    feat(Article Model): Default values
    Add some default values

--- blog-server/server/models/article.js
+++ blog-server/server/models/article.js
@@ -11,7 +11,8 @@ const articleSchema = new Schema({
   class: Array,
   tags: Array,
   meta: {
-    time: String,
+    create_time: Date,
+    update_time: Date,
     count: {
       comment: Number,
       word: Number,
@@ -19,7 +20,27 @@ const articleSchema = new Schema({
       likes: Number
     }
   },
-  content: ''
+  content: {
+    type: String,
+    default: ''
+  }
+})
+
+articleSchema.pre('save', function (next) {
+  if (this.isNew) {
+    const time = Date.now()
+    this.meta = {
+      create_time: time,
+      update_time: time,
+      count: {
+        comment: 0,
+        word: this.content.length || 0,
+        reading: 0,
+        likes: 0
+      }
+    }
+  }
+  next()
 })
 
 module.exports = mongoose.model('article', articleSchema)
